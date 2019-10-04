a64052caa036748847d302775c8eeb8ec15ab947
MachineLP <MachineLP@163.com>
2019-10-05 07:08:32 +0800

    init

---
+++ 03-deeplearning_examples/01-tensorflow_examples/04-CV/01-classification/train_cnn/lib/loss/loss.py
@@ -0,0 +1,153 @@
+# coding=utf-8
+
+import tensorflow as tf 
+import numpy as np 
+
+
+# quared loss
+def squared_loss(label, logit):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(label - logit), 1))
+    return loss
+
+# sigmoid loss
+def sigmoid_loss(label, logit):
+    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels = label, logits = logit))
+    return loss
+
+# softmax loss
+def softmax_loss(label, logit):
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = label, logits = logit))
+    return loss
+
+# triplet loss 
+def triplet_loss(anchor, positive, negative, alpha):
+    # 理论可以看这里： https://blog.csdn.net/tangwei2014/article/details/46788025
+    # facenet理解： http://www.mamicode.com/info-detail-2096766.html
+    """Calculate the triplet loss according to the FaceNet paper
+    
+    Args:
+      anchor: the embeddings for the anchor images.
+      positive: the embeddings for the positive images.
+      negative: the embeddings for the negative images.
+  
+    Returns:
+      the triplet loss according to the FaceNet paper as a float tensor.
+    """
+    with tf.variable_scope('triplet_loss'):
+        pos_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, positive)), 1)
+        neg_dist = tf.reduce_sum(tf.square(tf.subtract(anchor, negative)), 1)
+        
+        basic_loss = tf.add(tf.subtract(pos_dist,neg_dist), alpha)
+        loss = tf.reduce_mean(tf.maximum(basic_loss, 0.0), 0)
+      
+    return loss
+
+
+# center loss
+def center_loss(features, label, alfa, nrof_classes):
+    """Center loss based on the paper "A Discriminative Feature Learning Approach for Deep Face Recognition"
+       (http://ydwen.github.io/papers/WenECCV16.pdf)
+    """
+    """获取center loss及center的更新op 
+    还可参考博客： https://blog.csdn.net/u014365862/article/details/79184966     
+    Arguments: 
+        features: Tensor,表征样本特征,一般使用某个fc层的输出,shape应该为[batch_size, feature_length]. 
+        labels: Tensor,表征样本label,非one-hot编码,shape应为[batch_size]. 
+        alpha: 0-1之间的数字,控制样本类别中心的学习率,细节参考原文. 
+        num_classes: 整数,表明总共有多少个类别,网络分类输出有多少个神经元这里就取多少. 
+     
+    Return： 
+        loss: Tensor,可与softmax loss相加作为总的loss进行优化. 
+        centers: Tensor,存储样本中心值的Tensor，仅查看样本中心存储的具体数值时有用. 
+        centers_update_op: op,用于更新样本中心的op，在训练时需要同时运行该op，否则样本中心不会更新 
+    """  
+    nrof_features = features.get_shape()[1]
+    centers = tf.get_variable('centers', [nrof_classes, nrof_features], dtype=tf.float32,
+        initializer=tf.constant_initializer(0), trainable=False)
+    label = tf.reshape(label, [-1])
+    centers_batch = tf.gather(centers, label)
+    diff = (1 - alfa) * (centers_batch - features)
+    centers = tf.scatter_sub(centers, label, diff)
+    loss = tf.reduce_mean(tf.square(features - centers_batch))
+    return loss, centers
+
+
+# 加入L2正则化的loss
+def add_l2(loss, weight_decay):
+    l2_losses = [weight_decay * tf.nn.l2_loss(v) for v in tf.trainable_variables() if 'weights' in v.name]
+    reduced_loss = tf.reduce_mean(loss) + tf.add_n(l2_losses)
+    return reduced_loss
+
+'''使用
+    w_init_method = tf.contrib.layers.xavier_initializer(uniform=False)
+    net = get_resnet(images, args.net_depth, type='ir', w_init=w_init_method, trainable=True, keep_rate=dropout_rate)
+    # 3.2 get arcface loss
+    logit = arcface_loss(embedding=net.outputs, labels=labels, w_init=w_init_method, out_num=args.num_output)
+    # test net  because of batch normal layer
+    tl.layers.set_name_reuse(True)
+    test_net = get_resnet(images, args.net_depth, type='ir', w_init=w_init_method, trainable=False, reuse=True, keep_rate=dropout_rate)
+    embedding_tensor = test_net.outputs
+    # 3.3 define the cross entropy
+    inference_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logit, labels=labels))
+'''
+def arcface_loss(embedding, labels, out_num, w_init=None, s=64., m=0.5):
+    '''
+    :param embedding: the input embedding vectors
+    :param labels:  the input labels, the shape should be eg: (batch_size, 1)
+    :param s: scalar value default is 64
+    :param out_num: output class num
+    :param m: the margin value, default is 0.5
+    :return: the final cacualted output, this output is send into the tf.nn.softmax directly
+    '''
+    cos_m = math.cos(m)
+    sin_m = math.sin(m)
+    mm = sin_m * m  # issue 1
+    threshold = math.cos(math.pi - m)
+    with tf.variable_scope('arcface_loss'):
+        # inputs and weights norm
+        embedding_norm = tf.norm(embedding, axis=1, keep_dims=True)
+        embedding = tf.div(embedding, embedding_norm, name='norm_embedding')
+        weights = tf.get_variable(name='embedding_weights', shape=(embedding.get_shape().as_list()[-1], out_num),
+                                  initializer=w_init, dtype=tf.float32)
+        weights_norm = tf.norm(weights, axis=0, keep_dims=True)
+        weights = tf.div(weights, weights_norm, name='norm_weights')
+        # cos(theta+m)
+        cos_t = tf.matmul(embedding, weights, name='cos_t')
+        cos_t2 = tf.square(cos_t, name='cos_2')
+        sin_t2 = tf.subtract(1., cos_t2, name='sin_2')
+        sin_t = tf.sqrt(sin_t2, name='sin_t')
+        cos_mt = s * tf.subtract(tf.multiply(cos_t, cos_m), tf.multiply(sin_t, sin_m), name='cos_mt')
+
+        # this condition controls the theta+m should in range [0, pi]
+        #      0<=theta+m<=pi
+        #     -m<=theta<=pi-m
+        cond_v = cos_t - threshold
+        cond = tf.cast(tf.nn.relu(cond_v, name='if_else'), dtype=tf.bool)
+
+        keep_val = s*(cos_t - mm)
+        cos_mt_temp = tf.where(cond, cos_mt, keep_val)
+
+        mask = tf.one_hot(labels, depth=out_num, name='one_hot_mask')
+        # mask = tf.squeeze(mask, 1)
+        inv_mask = tf.subtract(1., mask, name='inverse_mask')
+
+        s_cos_t = tf.multiply(s, cos_t, name='scalar_cos_t')
+
+        output = tf.add(tf.multiply(s_cos_t, inv_mask), tf.multiply(cos_mt_temp, mask), name='arcface_loss_output')
+    return output
+
+def smoothing_loss(one_hot_labels, logits, label_smoothing=0.1, weight=1.0, scope=None):
+    logits.get_shape().assert_is_compatible_with(one_hot_labels.get_shape())
+    with tf.name_scope(scope, 'CrossEntropyLoss', [logits, one_hot_labels]):
+        num_classes = one_hot_labels.get_shape()[-1].value
+        print ('>>>>>>>>>>>>>>', num_classes)
+        one_hot_labels = tf.cast(one_hot_labels, logits.dtype)
+        if label_smoothing > 0:
+            smooth_positives = 1.0 - label_smoothing
+            smooth_negatives = label_smoothing / num_classes
+            one_hot_labels = one_hot_labels * smooth_positives + smooth_negatives
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits,labels=one_hot_labels, name='xentropy')
+        weight = tf.convert_to_tensor(weight, dtype=logits.dtype.base_dtype, name='loss_weight')
+        loss = tf.multiply(weight, tf.reduce_mean(cross_entropy), name='value')
+        # tf.add_to_collection(LOSSES_COLLECTION, loss)
+        return loss
